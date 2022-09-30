b6343cb642677e8f6548f83fe99d3440a0e9aa20
Peter Fontaine <peter.fontaine@reactiva.fr>
2022-09-30 16:53:56 +0200

    feat: add network

--- network.tf
+++ network.tf
@@ -22,6 +22,24 @@ resource "aws_subnet" "mp_subnet_private" {
   cidr_block = "10.0.128.0/20"
 }
 
+resource "aws_route_table" "mp_route_table" {
+  vpc_id = aws_vpc.mp_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mp_igw.id
+  }
+
+  tags = {
+    Name = "mayaprotect_route_table"
+  }
+}
+
+resource "aws_main_route_table_association" "mp_default_route_table_association" {
+  route_table_id = aws_route_table.mp_route_table.id
+  vpc_id = aws_vpc.mp_vpc.id
+}
+
 resource "aws_internet_gateway" "mp_igw" {
   vpc_id = aws_vpc.mp_vpc.id
 }
