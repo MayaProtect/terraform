b4a58da6e058fb9e5e837f74dd564afd223abb90
Peter Fontaine <peter.fontaine@reactiva.fr>
2022-10-07 11:40:31 +0200

    feat: add dns output and ansible inventory output

--- output.tf
+++ output.tf
@@ -17,3 +17,16 @@ output "ip_public_ssh_gateway" {
 output "efs_dns" {
   value = aws_efs_mount_target.mp_efs_mount_target.dns_name
 }
+
+output "dns_servers" {
+  value = aws_route53_zone.public_zone.name_servers
+}
+
+resource "local_file" "ansible-inventory" {
+  content = templatefile("${path.module}/inventory.tpl", {
+    master_ip = aws_instance.mp_master.private_ip
+    nodes_ips = { for k, v in aws_instance.mp_nodes : k => v.private_ip }
+    gateway_ip = aws_eip.mp_tool_gateway_eip.public_ip
+  })
+  filename = "${path.module}/hosts.ini"
+}
